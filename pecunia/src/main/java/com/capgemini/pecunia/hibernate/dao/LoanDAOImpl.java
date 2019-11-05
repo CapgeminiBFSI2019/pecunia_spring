4280e33bc996da524236a34d3cd7dba666c63d0a
risrai <55137949+risrai@users.noreply.github.com>
2019-11-05 15:32:45 +0530

    format done in loan request part

--- pecunia/src/main/java/com/capgemini/pecunia/hibernate/dao/LoanDAOImpl.java
+++ pecunia/src/main/java/com/capgemini/pecunia/hibernate/dao/LoanDAOImpl.java
@@ -2,7 +2,6 @@ package com.capgemini.pecunia.hibernate.dao;
 
 import org.apache.log4j.Logger;
 import org.hibernate.Session;
-import org.hibernate.Transaction;
 import org.springframework.stereotype.Repository;
 
 import com.capgemini.pecunia.dto.Loan;
@@ -11,24 +10,25 @@ import com.capgemini.pecunia.exception.ErrorConstants;
 import com.capgemini.pecunia.exception.LoanException;
 import com.capgemini.pecunia.exception.PecuniaException;
 import com.capgemini.pecunia.util.HibernateUtil;
+
 @Repository
 public class LoanDAOImpl implements LoanDAO {
 	Logger logger = Logger.getRootLogger();
 
 	@Override
 	public int addLoanDetails(Loan loan) throws PecuniaException, LoanException {
-		
+
 		int loanId = 0;
 		org.hibernate.Transaction txn = null;
 		try {
 			Session session = HibernateUtil.getSessionFactory().openSession();
-			
-			 txn = session.beginTransaction();
+
+			txn = session.beginTransaction();
 			LoanRequestEntity loanRequestEntity = new LoanRequestEntity();
 			loanRequestEntity.setAccountId(loan.getAccountId());
 			loanRequestEntity.setAmount(loan.getAmount());
 			loanRequestEntity.setCreditScore(loan.getCreditScore());
-			loanRequestEntity.setEmi(loan.getEmi());
+			loanRequestEntity.setEmi(loan.getEmi());	
 			loanRequestEntity.setRoi(loan.getRoi());
 			loanRequestEntity.setTenure(loan.getTenure());
 			loanRequestEntity.setType(loan.getType());
@@ -38,9 +38,8 @@ public class LoanDAOImpl implements LoanDAO {
 			loanId = loanRequestEntity.getLoanId();
 			txn.commit();
 
-
 		} catch (Exception e) {
-			if(txn != null) {
+			if (txn != null) {
 				txn.rollback();
 			}
 			logger.error(e.getMessage());
